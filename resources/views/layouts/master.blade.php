7d095fc1a078d06e58831a5fecd8ce20332b675e
MyukaQA <mylian214@gmail.com>
2020-12-28 22:26:09 +0700

    update layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,7 +32,7 @@
           <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></li>
+                  
                   @guest
                     <li class="nav-item"><a class="nav-link" href="{{route('login')}}">Masuk</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{route('register')}}">Daftar</a></li>
@@ -65,7 +65,7 @@
 
 @yield('content')
 
-  <footer class="bg-dark pb-5">
+  <footer class="pb-5" style="background-color: #a7c5eb">
     <div class="container text-center">
       <p class="font-italic mb-0 text-white">&copy; Copyrights <b class="font-weight-bold">Kelompok 1 UAS Framework</b>.</p>
     </div>
